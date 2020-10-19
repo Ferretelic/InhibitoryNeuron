5ac9290843b27941909f462402379d05d3f75cbf
DOALA0155 <kyleshouki@gmail.com>
2020-10-19 21:34:46 +0900

    compared dog cat

--- DogCatTrain/Comparison/compare_histories.py
+++ DogCatTrain/Comparison/compare_histories.py
@@ -4,15 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 histories = []
+models = [
+  "NormalRelu",
+  "DistributionKReluGradientMuRandom",
+  "DistributionKReluGradientMuRandomIncreased",
+  "DistributionKReluGradientMuVarianceRandom",
+  "DistributionKReluGradientMuVarianceRandomAbsolute"
+]
 
 for history_type in ["accuracy", "loss"]:
   plt.figure()
-  for directory in os.listdir("../"):
+  for index, directory in enumerate(models):
     path = os.path.join("../", directory, "model", "model_history.pkl")
     if os.path.exists(path) == True:
       with open(path, "rb") as f:
         history = pickle.load(f)
-        plt.plot(np.arange(len(history[history_type])), history[history_type], label=directory)
+        plt.plot(np.arange(len(history[history_type])), history[history_type], label="Model{}".format(index + 1))
 
   plt.xlabel("epochs")
   plt.ylabel(history_type)
