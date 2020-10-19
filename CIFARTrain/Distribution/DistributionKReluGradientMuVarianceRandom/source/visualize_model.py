5cbd7b278502af5d1219ca5c53a1052e602461b7
DOALA0155 <kyleshouki@gmail.com>
2020-10-20 00:22:23 +0900

    added visualized model

--- CIFARTrain/Distribution/DistributionKReluGradientMuVarianceRandom/source/visualize_model.py
+++ CIFARTrain/Distribution/DistributionKReluGradientMuVarianceRandom/source/visualize_model.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 
 model = DistributionConvolutionModelKReluGradientMuRandom()
 model.load_state_dict(torch.load("../model/model_final.pth")["model_state_dict"])
-distributions = model.conv3_1.distribution.detach().numpy()
+distributions = model.conv1_2.distribution.detach().numpy()
 
-figure, graphs = plt.subplots(8, 16)
-for graph, distribution in zip(graphs.reshape(128), distributions):
+figure, graphs = plt.subplots(4, 8)
+for graph, distribution in zip(graphs.reshape(32), distributions):
   graph.matshow(distribution)
   graph.set_xticklabels([])
   graph.set_yticklabels([])
   graph.axis("off")
 
 plt.subplots_adjust(wspace=0, hspace=0)
-figure.savefig("../image/distribution_trained_conv3_1.png")
+figure.savefig("../image/distribution_trained_conv1_2.png")
 
-plot_distribution(distributions[0], name="distribution_trained.png")
-plot_distribution_2d(distributions[0], name="distribution_trained_2d.png")
+# plot_distribution(distributions[0], name="distribution_trained.png")
+# plot_distribution_2d(distributions[0], name="distribution_trained_2d.png")
