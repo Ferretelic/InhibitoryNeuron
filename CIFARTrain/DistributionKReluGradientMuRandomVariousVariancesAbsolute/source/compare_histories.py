10e2da67f63e3076bfe9e34148658af26fef3475
DOALA0155 <kyleshouki@gmail.com>
2020-10-19 23:40:44 +0900

    compared variaces

---
+++ CIFARTrain/DistributionKReluGradientMuRandomVariousVariancesAbsolute/source/compare_histories.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import os
+
+def show_compare_histories(path_list, name_list, variance_list):
+  accuracies = []
+  losses = []
+
+  for path, name in zip(path_list, name_list):
+    history_path = os.path.join(path, name)
+    with open(history_path, "rb") as f:
+      history = pickle.load(f)
+      accuracies.append(history["accuracy"])
+      losses.append(history["loss"])
+
+  plt.figure()
+  for index, accuracy in enumerate(accuracies):
+    plt.plot(np.arange(len(accuracy)), accuracy)
+  plt.legend(variance_list)
+  plt.savefig("../image/accuracy.png")
+
+  plt.figure()
+  for loss in losses:
+    line = plt.plot(np.arange(len(loss)), loss)
+  plt.legend(name_list)
+  plt.savefig("../image/loss.png")
+
+name_list = []
+path_list = []
+variance_list = []
+
+for i in np.linspace(0.1, 1, 10):
+  i = np.round(i, decimals=1)
+
+  name = "model_history_{}.pkl".format(i)
+  path = "../model"
+
+  name_list.append(name)
+  path_list.append(path)
+  variance_list.append("variance: {}".format(i))
+
+show_compare_histories(path_list, name_list, variance_list)
