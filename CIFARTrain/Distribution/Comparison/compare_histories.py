4dd700fc279b5f8557abc061bdc8183c0b9bc8a9
DOALA0155 <kyleshouki@gmail.com>
2020-10-19 21:09:38 +0900

    compared models

--- CIFARTrain/Distribution/Comparison/compare_histories.py
+++ CIFARTrain/Distribution/Comparison/compare_histories.py
@@ -5,14 +5,25 @@ import numpy as np
 
 histories = []
 
+models = [
+  "NormalRelu",
+  "NormalReluReduced",
+  "DistributionKReluGradientMuRandom","DistributionKReluGradientMuRandomReduced",
+  "DistributionKReluGradientMuRandomIncreased",
+  "DistributionKReluGradientMuVarianceRandom",
+  "DistributionKReluGradientMuVarianceRandomReduced",
+  "DistributionKReluGradientMuVarianceRandomAbsolute",
+  "DistributionKReluGradientMuVarianceRandomAbsoluteReduced"
+]
+
 for history_type in ["accuracy", "loss"]:
   plt.figure()
-  for directory in os.listdir("../"):
+  for index, directory in enumerate(models):
     path = os.path.join("../", directory, "model", "model_history.pkl")
     if os.path.exists(path) == True:
       with open(path, "rb") as f:
         history = pickle.load(f)
-        plt.plot(np.arange(len(history[history_type])), history[history_type], label=directory)
+        plt.plot(np.arange(len(history[history_type])), history[history_type], label="Model{}".format(index + 1))
 
   plt.xlabel("epochs")
   plt.ylabel(history_type)
