a26a383344993c6d4d92ad104cc4710c210a94ea
DOALA0155 <kyleshouki@gmail.com>
2020-10-21 17:03:17 +0900

    initial distribution

--- CIFARTrain/Distribution/DistributionKReluGradientMuRandom/source/visualize_model.py
+++ CIFARTrain/Distribution/DistributionKReluGradientMuRandom/source/visualize_model.py
@@ -5,17 +5,33 @@ import matplotlib.pyplot as plt
 
 model = DistributionConvolutionModelKReluGradientMuRandom()
 model.load_state_dict(torch.load("../model/model_final.pth")["model_state_dict"])
-distributions = model.conv1_1.distribution.detach().numpy()
 
-figure, graphs = plt.subplots(4, 8)
-for graph, distribution in zip(graphs.reshape(32), distributions):
-  graph.matshow(distribution)
-  graph.set_xticklabels([])
-  graph.set_yticklabels([])
-  graph.axis("off")
+layers = [
+  [model.conv1_1, model.conv1_2],
+  [model.conv2_1, model.conv2_2],
+  [model.conv3_1, model.conv3_2]
+]
+shapes = [[4, 8], [8, 8], [8, 16]]
 
-plt.subplots_adjust(wspace=0, hspace=0)
-figure.savefig("../image/distribution_trained_conv1_1.png")
+for i, layer in enumerate(layers):
+  for j, conv in enumerate(layer):
+    print("conv{}_{}.png".format(i + 1, j + 1))
+    distributions = conv.distribution.detach().numpy()
+    print(distributions.shape)
 
-plot_distribution(distributions[0], name="distribution_trained.png")
-plot_distribution_2d(distributions[0], name="distribution_trained_2d.png")
+    figure, graphs = plt.subplots(*shapes[i])
+    print(distributions.shape)
+    print(graphs.shape)
+    print(shapes[i][0] * shapes[i][1])
+
+    for graph, distribution in zip(graphs.reshape(shapes[i][0] * shapes[i][1]), distributions):
+      graph.matshow(distribution)
+      graph.set_xticklabels([])
+      graph.set_yticklabels([])
+      graph.axis("off")
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    figure.savefig("../image/distribution_trained_conv{}_{}.png".format(i + 1, j + 1))
+
+# plot_distribution(distributions[0], name="distribution_trained.png")
+# plot_distribution_2d(distributions[0], name="distribution_trained_2d.png")
